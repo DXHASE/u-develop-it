071365f02751669f400cf087f92108e2edd5e114
DXHASE <danielchas886@gmail.com>
2022-10-07 17:59:31 -0500

    Built database calls

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //Express middleware
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.json({
-        message: 'Hello World'
-    })
+//Connect to database (MySQL)
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        user:'root',
+        password:'Apaches0',
+        database:'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
+
+db.query(`SELECT * FROM candidates`, (err,rows) => {
+    console.log(rows);
 });
 
 //Default response for any other request(Not Found)
-app.use((req,res)=>{
-res.status(404).end();
+app.use((req,res) => {
+    res.status(404).end();
 });
-
-
-app.listen(PORT, () => {
-    console.log(`server running on ${PORT}`);
-});
-
