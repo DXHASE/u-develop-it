edfd75047b56dab41d6081616c136cb084960d0f
DXHASE <danielchas886@gmail.com>
2022-10-08 14:49:22 -0500

    Added query blocks

--- server.js
+++ server.js
@@ -23,8 +23,38 @@ app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
 
-db.query(`SELECT * FROM candidates`, (err,rows) => {
-    console.log(rows);
+//GET a list of all candidates
+// db.query(`SELECT * FROM candidates`, (err,rows) => {
+//     console.log(rows);
+// });
+
+//GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err,row) => {
+//     if(err){
+//         console.log(err)
+//     }
+//     console.log(row);
+// });
+
+//Delete a candidate   | 1 == extra param that "?" references
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, results) => {
+// if(err){
+//     console.log(err)
+// }
+// console.log(results);
+// });
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+VALUES (?,?,?,?)`;
+
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params,(err,result) => {
+if(err){
+    console.log(err)
+}
+console.log(result);
 });
 
 //Default response for any other request(Not Found)
